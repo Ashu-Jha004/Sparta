4d231c4a0ac12050cb1d04831d2877fa9843b7a3
ashu jha <silliconvally009@gmail.com>
2025-05-18 21:18:48 +0530

    Update SEO, robots.txt, sitemap config, and footer email

--- public/next-sitemap.config.js
+++ public/next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const config = {
-  siteUrl: process.env.SITE_URL || "https://yourdomain.com",
+  siteUrl: process.env.SITE_URL || "https://sparta009.vercel.app/",
   generateRobotsTxt: true, // (optional)
   // ...other options
 };
